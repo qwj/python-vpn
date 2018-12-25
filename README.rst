7eb9aae8d7b206a6e8153019a2b3f38b25348179
Qian Wenjie <qianwenjie@gmail.com>
2018-12-25 20:07:45 +0800

    initial upload

---
+++ README.rst
@@ -0,0 +1,40 @@
+python-vpn
+============
+
+|made-with-python| |PyPI-version| |Hit-Count|
+
+.. |made-with-python| image:: https://img.shields.io/badge/Made%20with-Python-1f425f.svg
+   :target: https://www.python.org/
+.. |PyPI-version| image:: https://badge.fury.io/py/pvpn.svg
+   :target: https://pypi.python.org/pypi/pvpn/
+.. |Hit-Count| image:: http://hits.dwyl.io/qwj/python-vpn.svg
+   :target: https://pypi.python.org/pypi/pvpn/
+
+IKEv2+SPE+NAT Layer3 VPN implemented in pure Python.
+
+Introduction
+------------
+
+All VPN softwares are stupid, clumsy and hard to configure. So comes **python-vpn**.
+
+QuickStart
+----------
+
+.. code:: rst
+
+  $ pip3 install pvpn
+  Successfully installed pvpn-0.0.1
+  $ pvpn -p yourpassword
+  Serving on UDP :500 :4500...
+  ^C
+
+Open the UDP port :500 :4500 to your device. Add "IKEv2 (iOS)"" or "IPSec IKEcv2 PSK (Android)" VPN, write down the id "test" and password "yourpassword". Connect.
+
+You should change the default password to keep higher security. See "pvpn -h" for more options.
+
+Features
+--------
+
+- IKEv2+SPE+NAT Layer3
+- Tunnel TCP/UDP to your proxy
+
---
+++ setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup
+import os, re
+
+def read(*names, **kwargs):
+    with open(os.path.join(os.path.dirname(__file__), *names), encoding='utf8') as fp:
+        return fp.read()
+
+def find_value(name):
+    data_file = read('pvpn', '__doc__.py')
+    data_match = re.search(r"^__%s__ += ['\"]([^'\"]*)['\"]" % name, data_file, re.M)
+    if data_match:
+        return data_match.group(1)
+    raise RuntimeError(f"Unable to find '{name}' string.")
+
+setup(
+    name                = find_value('title'),
+    version             = find_value('version'),
+    description         = find_value('description'),
+    long_description    = read('README.rst'),
+    url                 = find_value('url'),
+    author              = find_value('author'),
+    author_email        = find_value('email'),
+    license             = find_value('license'),
+    python_requires     = '>=3.6',
+    keywords            = find_value('keywords'),
+    packages            = ['pvpn'],
+    classifiers         = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    install_requires    = [
+        'pycryptodome >= 3.4.3',
+        'pproxy >= 2.0.0',
+    ],
+    entry_points        = {
+        'console_scripts': [
+            'pvpn = pvpn.server:main',
+        ],
+    },
+)
