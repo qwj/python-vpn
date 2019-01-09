054b7785dfe9d96e3cf077bf83385236e7e72df1
qwj <qianwenjie@gmail.com>
2019-01-09 13:04:06 +0800

    change name

--- pesp/__doc__.py
+++ pesp/__doc__.py
@@ -1,10 +1,10 @@
-__title__       = "pike"
+__title__       = "pesp"
 __version__     = "0.1.4"
 __license__     = "MIT"
-__description__ = "IKE/ESP server"
-__keywords__    = "ike spe ip tcp udp"
+__description__ = "Python encapsulating security payload server."
+__keywords__    = "ike esp ip tcp udp"
 __author__      = "Qian Wenjie"
 __email__       = "qianwenjie@gmail.com"
-__url__         = "https://github.com/qwj/python-ike"
+__url__         = "https://github.com/qwj/python-esp"
 
 __all__ = ['__title__', '__version__', '__description__', '__url__']
